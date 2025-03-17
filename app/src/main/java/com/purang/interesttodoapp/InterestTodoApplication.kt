49b94b160f20f69f941250a2daa4230d890b130f
sonms <smsms5676@naver.com>
2025-03-17 16:44:07 +0900

    feat/first-commit

---
+++ app/src/main/java/com/purang/interesttodoapp/InterestTodoApplication.kt
@@ -0,0 +1,8 @@
+package com.purang.interesttodoapp
+
+import android.app.Application
+import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class InterestTodoApplication : Application()
---
+++ app/src/main/java/com/purang/interesttodoapp/ui/theme/Color.kt
@@ -0,0 +1,29 @@
+package com.purang.interesttodoapp.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val BluePrimary = Color(0xFF007AFF) // 신뢰감 있는 파란색
+val GreenSecondary = Color(0xFF34C759) // 할 일 완료 버튼용 녹색
+val BackgroundLight = Color(0xFFF5F5F5) // 연한 회색 배경
+val CardWhite = Color(0xFFFFFFFF) // 카드 배경용 화이트
+val TextPrimary = Color(0xFF1C1C1E) // 기본 텍스트 (진한 검정)
+val TextSecondary = Color(0xFF8E8E93) // 보조 텍스트 (연한 회색)
+val ErrorRed = Color(0xFFFF3B30) // 경고 메시지 (빨간색)
+val AccentOrange = Color(0xFFFF9500) // 포인트 강조색 (오렌지)
+
+val BlueP1 = Color(0xFFF0EFFF) // 가장 밝은 블루 (배경)
+val BlueP2 = Color(0xFFDEDCFF) // 부드러운 라벤더 톤
+val BlueP3 = Color(0xFFCBC9FF) // 연한 블루 느낌
+val BlueP4 = Color(0xFFB8B6FF) // 메인으로 가는 단계
+val BlueP5 = Color(0xFFA5A3FE) // 메인 직전
+val BlueP6 = Color(0xFF8281CC) // 메인 컬러
+val BlueP7 = Color(0xFF626199) // 다크 블루
+val BlueP8 = Color(0xFF414066) // 가장 어두운 블루
